300ba7644248ed942ce1ce354ef20c48b0b7cfb9
JongKeun Choi <jkchoi@nexell.co.kr>
2016-08-31 11:11:47 +0900

    testsuite: dp_decimator_crop_n_scaledown_test: Add clipping and scale-down of decimator test App
    This patch adds test App for clipping and scale-down through decimator.
    video image of the clipper is output through the LCD, video images of
    the decimator is output through HDMI. This app is tested
    input image of camera operation, use the following library.
    
        . Using the library
                - nx-drm-allocator
                - nx-v4l2 - nx-renderer
                - kms
                - drm
                - pthread
    
    	test command>
    		dp_decimator_crop_n_scaledown_test -m <MODULE_NUMBER> -w <WIDTH>
    		-h <HEIGHT> -C <CROP X>,<CROP Y>,<CROP WIDTH>,<CROP HEIGHT>
    		-W <SCALE DOWN WIDTH> -H <SCALE DOWN HEIGHT>
    		-f <VIDEO FORMAT> -F <MEDIA BUS FORMAT> -c <TEST-LOOP-COUNT>
    
    	ex)
            * cropping test
    		dp_decimator_crop_n_scaledown_test -m 0 -w 1280 -h 720 -C 100,100,640,480 -f 0 -F 0 -c 1000
    
    	*. cropping & scaledown
    		dp_decimator_crop_n_scaledown_test -m 0 -w 1280 -h 720 -C 10,10,640,480 -W 320 -H 240 -f 0 -F 0 -c 1000
    
    Change-Id: Ie17355276aeee5603e8de7c9077e26fd1dc8d705
    Signed-off-by: JongKeun Choi <jkchoi@nexell.co.kr>

---
+++ dp_decimator_crop_n_scaledown_test/option.h
@@ -0,0 +1,23 @@
+#ifndef _OPTION_H
+#define _OPTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct rect {
+	int x;
+	int y;
+	int width;
+	int height;
+};
+
+int handle_option(int argc, char **argv, uint32_t *m, uint32_t *w, uint32_t *h,
+	uint32_t *W, uint32_t *H, uint32_t *f, uint32_t *bus_f,
+	uint32_t *count, struct rect *C);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

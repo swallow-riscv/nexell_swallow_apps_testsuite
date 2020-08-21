7db0473b707a3f8c86bf2268585518d94220224a
Hyunseok Jung <hsjung@coasianexell.com>
2020-08-21 09:09:37 +0900

    testsuite: test apps : change uart test app for loopback
    This patch changes uart test app for loopback.
    
    Signed-off-by: Hyunseok Jung <hsjung@coasianexell.com>
    Change-Id: I22a66fbd6aacde53c10f9c6846ea1973a41a2614
    Reviewed-on: https://review.gerrithub.io/c/swallow-riscv/nexell_swallow_apps_testsuite/+/501072

--- uart/uart_test.c
+++ uart/uart_test.c
@@ -29,7 +29,7 @@
 struct test_data
 {
 	unsigned char * pbuf;
-	int 			fd;
+	int		fd;
 };
 
 static struct sig_param s_par;
@@ -52,31 +52,34 @@ void print_usage(void)
 {
 	 printf("usage: options\n"
             " -p tty name                       , default %s 			\n"
+            " -d tty name                       , default %s 			\n"
             " -b baudrate                       , default %d 			\n"
+            " -h print this message \n"
             /*
-        	" -c canonical                      , default non 			\n"
-        	" -i timeout                        , default %d 			\n"
-        	" -n min char                       , default %d 			\n"
-        	" -t 2 stopbit                      , default 1 stop bit 	\n"
-        	" -a set partiy bit                 , default no parity		\n"
-        	" -o odd parity                     , default even parity 	\n"
-        	" -u result file name               , default %s 			\n"
-        	" -f h/w flow                       , default sw flow 		\n"
-        	//" -s test low baud rate             , default 2400			\n"
-        	//" -e test high baud rate            , default 115200		\n"
-        	" -r set random transfer max size   , default 1024			\n"
-			" -l set total test size (KByte)    , default 64			\n"
-        	" -r set data len 5,6,7,8 bit       , default 8				\n"
-        	" -m mode                           , default read_mode		\n"
-        	" 	0 : read mode , 1 : write mode , 						\n"
-        	" 	2 : test master mode  , 3 : test slave mode				\n"
-        	*/
-        	, TTY_NAME
-        	//, TTY_BAUDRATE
-        	//, TTY_NC_TIMEOUT
-        	//, TTY_NC_CHARNUM
-        	//, FILE_NAME
-        	);
+		" -c canonical                      , default non 			\n"
+		" -i timeout                        , default %d 			\n"
+		" -n min char                       , default %d 			\n"
+		" -t 2 stopbit                      , default 1 stop bit 	\n"
+		" -a set partiy bit                 , default no parity		\n"
+		" -o odd parity                     , default even parity 	\n"
+		" -u result file name               , default %s 			\n"
+		" -f h/w flow                       , default sw flow 		\n"
+		//" -s test low baud rate             , default 2400			\n"
+		//" -e test high baud rate            , default 115200		\n"
+		" -r set random transfer max size   , default 1024			\n"
+		" -l set total test size (KByte)    , default 64			\n"
+		" -r set data len 5,6,7,8 bit       , default 8				\n"
+		" -m mode                           , default read_mode		\n"
+		" 	0 : read mode , 1 : write mode , 						\n"
+		" 	2 : test master mode  , 3 : test slave mode				\n"
+		*/
+		, TTY_NAME
+		, TTY_NAME
+		//, TTY_BAUDRATE
+		//, TTY_NC_TIMEOUT
+		//, TTY_NC_CHARNUM
+		//, FILE_NAME
+		);
 }
 
 static int get_baudrate(int op_baudrate)
@@ -84,37 +87,37 @@ static int get_baudrate(int op_baudrate)
 	int speed;
 
 	switch (op_baudrate) {
-		case 0    	: speed =       B0; break;
-		case 50   	: speed =      B50; break;
-		case 75   	: speed =      B75; break;
-		case 110  	: speed =     B110; break;
-		case 134  	: speed =     B134; break;
-		case 150  	: speed =     B150; break;  
-		case 200  	: speed =     B200; break;
-		case 300  	: speed =     B300; break;
-		case 600  	: speed =     B600; break;
-		case 1200 	: speed =    B1200; break;
-		case 1800 	: speed =    B1800; break;
-		case 2400 	: speed =    B2400; break;
-		case 4800 	: speed =    B4800; break;
-		case 9600 	: speed =    B9600; break;
+		case 0		: speed =       B0; break;
+		case 50		: speed =      B50; break;
+		case 75		: speed =      B75; break;
+		case 110	: speed =     B110; break;
+		case 134	: speed =     B134; break;
+		case 150	: speed =     B150; break;
+		case 200	: speed =     B200; break;
+		case 300	: speed =     B300; break;
+		case 600	: speed =     B600; break;
+		case 1200	: speed =    B1200; break;
+		case 1800	: speed =    B1800; break;
+		case 2400	: speed =    B2400; break;
+		case 4800	: speed =    B4800; break;
+		case 9600	: speed =    B9600; break;
 		case 19200	: speed =   B19200; break;
 		case 38400	: speed =   B38400; break;
-	  	case 57600  : speed =   B57600; break;
-  		case 115200 : speed =  B115200; break;
-  		case 230400 : speed =  B230400; break;
-  		case 460800 : speed =  B460800; break;
-  		case 500000 : speed =  B500000; break;
-  		case 576000 : speed =  B576000; break;
-  		case 921600 : speed =  B921600; break;
-  		case 1000000: speed = B1000000; break;
-  		case 1152000: speed = B1152000; break;
-  		case 1500000: speed = B1500000; break;
-  		case 2000000: speed = B2000000; break;
-  		case 2500000: speed = B2500000; break;
-  		case 3000000: speed = B3000000; break;
-  		case 3500000: speed = B3500000; break;
-  		case 4000000: speed = B4000000; break;
+		case 57600  : speed =   B57600; break;
+		case 115200 : speed =  B115200; break;
+		case 230400 : speed =  B230400; break;
+		case 460800 : speed =  B460800; break;
+		case 500000 : speed =  B500000; break;
+		case 576000 : speed =  B576000; break;
+		case 921600 : speed =  B921600; break;
+		case 1000000: speed = B1000000; break;
+		case 1152000: speed = B1152000; break;
+		case 1500000: speed = B1500000; break;
+		case 2000000: speed = B2000000; break;
+		case 2500000: speed = B2500000; break;
+		case 3000000: speed = B3000000; break;
+		case 3500000: speed = B3500000; break;
+		case 4000000: speed = B4000000; break;
 		default:
 			printf("Fail, not support op_baudrate, %d\n", op_baudrate);
 			return -1;
@@ -131,8 +134,8 @@ unsigned int recivedata(int fd, unsigned char * pbuf, int bufsize,int readsize)
 	unsigned int idx=0;
 	int size=0;
 	int timeout = 1;
-		
- 	while(size < bufsize)
+
+	while(size < bufsize)
 	{
 		FD_ZERO (&readfds);
 		FD_SET (fd, &readfds);
@@ -141,32 +144,32 @@ unsigned int recivedata(int fd, unsigned char * pbuf, int bufsize,int readsize)
 		len =0;
 
 		ret = select(fd + 1, &readfds, NULL, NULL, &tv);
-		       if (ret == -1) 
-        {
+		       if (ret == -1)
+		       {
            printf ("select(): error\n");
-        } 
-        else if (!ret) 
-        {
-        	printf ("%d seconds elapsed.\n", timeout );
-        	return -1;
         }
-        if (FD_ISSET (fd, &readfds)) 
+        else if (!ret)
         {
-     
-            len = read(fd, pbuf+idx, readsize);            
-            if (len == 0) 
+		printf ("%d seconds elapsed.\n", timeout );
+		return -1;
+        }
+        if (FD_ISSET (fd, &readfds))
+        {
+
+            len = read(fd, pbuf+idx, readsize);
+            if (len == 0)
 			{
 				UART_DBG("NULL\n");
 				continue;
 			}
-			if (0 > len) 
+			if (0 > len)
 			{
 				printf("Read error, %s\n", strerror(errno));
 				break;
 			}
 			size += len;
 			idx += len;
-			
+
 			if( (bufsize - size) < readsize )
 				readsize = bufsize - size ;
 		}
@@ -178,11 +181,11 @@ void *write_test_thread(void *data)
 {
 	struct test_data * wdata =  (struct test_data *)data;
 	unsigned char * tbuf = wdata->pbuf;
-	unsigned int 	size,idx,sendsize;	
+	unsigned int	size,idx,sendsize;
 	int fd = wdata->fd;
 	int ret;
 	size = 256;
-	
+
 	while(test_flag==0);
 
 	pthread_yield();
@@ -202,11 +205,10 @@ void *write_test_thread(void *data)
 		if(size < sendsize)
 			sendsize = size;
 	}while(size);
-	
-	UART_DBG("write end\n");		
+
+	UART_DBG("write end\n");
 	return 0;
 }
-
 
 int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 {
@@ -214,17 +216,17 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 	unsigned char * rbuf=NULL;
 	unsigned char * tbuf=NULL;
 	int size,i;
-		
+
 	struct test_data data;
-	
+
 	size = TEST_SIZE;
 	#if defined ( DUMP )
-	int fd_d;		
+	int fd_d;
 	static char s[256] = {0, };
 	#endif
 	tbuf = (unsigned char *)malloc(size*2);
 
-	UART_DBG("%s\n",__func__);		
+	UART_DBG("%s\n",__func__);
 	if(tbuf == NULL)
 	{
 		UART_ERR("Fail,aclloate tx buffer :  %d, %s\n",size , strerror(errno));
@@ -237,7 +239,7 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 
 	/* Make Test data for Tx */
 	srand(time(NULL));
-	
+
 		for(i=0;i<size;i++)
 		{
 			//tbuf[i]= rand()%255;
@@ -248,10 +250,10 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 
 			if(tbuf[i] ==126 || tbuf[i] ==127)
 				tbuf[i] = tbuf[i]+2;
-			
-			if(i%1024 == 1023 )	
+
+			if(i%1024 == 1023 )
 			{
-				tbuf[i] = 10;	
+				tbuf[i] = 10;
 			}
 */
 			if(i == size -2)
@@ -263,19 +265,18 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 				tbuf[i] = '\n';
 			}
 		}
-	
 
 	/* READ BUFFER Allocate For Rx Test */
-	rbuf = (unsigned char *)malloc(size* 2); 
+	rbuf = (unsigned char *)malloc(size* 2);
 	memset(rbuf,0,size*2);
 	if(rbuf == NULL)
 	{
 		UART_ERR("Fail, malloc %d, %s\n",size , strerror(errno));
 		goto _fail;
-		
+
 	}
 	UART_DBG("rbuf=0x%p \n", rbuf);
-	
+
 	if(pthread_create(&m_thread,NULL, write_test_thread, (void *)&data) < 0)
 	{
 		UART_ERR("Fail Create Thread");
@@ -284,36 +285,35 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 
 	test_flag = 1;
 
-	//	Wait Message Data for 3seconds
+	// Wait Message Data for 3seconds
 	ret = recivedata(fd1,rbuf,size, 256);
 	if(!ret)
-	{	
+	{
 		UART_DBG("test end \n");
 		UART_DBG("buf=0x%p:0x%p \n", tbuf,rbuf);
-		
+
 			ret = memcmp(tbuf, rbuf, size);
 			UART_DBG("buf=0x%px0x%p \n", tbuf,rbuf);
 			#if defined ( DUMP )
-			if(ret)
-			{	
-				fd_d = open("/mnt/mmc0/uart_test.txt",O_RDWR | O_APPEND);
+			//if(ret)
+			//{
+			//	fd_d = open("/mnt/mmc0/uart_test.txt",O_RDWR | O_APPEND);
 				for(i=0;i<size;i++)
 				{
 					UART_DBG("[%3d = 0x%02x  0x%02x ]\n",i, tbuf[i], rbuf[i]);
-					sprintf(s,"[%3d = 0x%02x  0x%02x ]\n",i, tbuf[i], rbuf[i]);
-					write(fd_d,s,strlen(s) );
+					//sprintf(s,"[%3d = 0x%02x  0x%02x ]\n",i, tbuf[i], rbuf[i]);
+					//write(fd_d,s,strlen(s) );
 				}
-							
-				close(fd_d);
-				sync();
-			}
+
+			//	close(fd_d);
+			//	sync();
+			//}
 			#endif
-		
+
 		UART_MSG(" Test ret = %d\n",ret);
 	}
 
 	pthread_join(m_thread,NULL);
-	
 
 	if(rbuf)
 		free(rbuf);
@@ -322,15 +322,15 @@ int test_transfer(int fd, int mode,int sbaud,int ebaud, int fd1)
 
 if(ret == 0)
 	return 0;
-else 
+else
 	return -1;
  _fail:
 
- 	if(rbuf)
+	if(rbuf)
 		free(rbuf);
 	if(tbuf)
 		free(tbuf);
-	
+
 	pthread_cancel(m_thread);
 
 	return -1;
@@ -346,7 +346,7 @@ int main (int argc, char **argv)
 	char ttypath[64] = TTY_NAME;
 	char ttypath1[64] = TTY_NAME;
 	char *pbuf = NULL;
-	
+
 	int op_baudrate = TTY_BAUDRATE;
 	int op_canoical = NON_CANOICAL;				//Canonical mode Default non
 	char op_mode = READ_MODE;
@@ -362,18 +362,17 @@ int main (int argc, char **argv)
 	int op_odd =0;
 	int op_data = 8;
 	int ret=0;
-	
 
 	pthread_mutex_init( &mutex, NULL );
 	pthread_cond_init( &cond, NULL );
 
-	
 	//while(-1 != (opt = getopt(argc, argv, "hp:b:m:ctafol:d:u:n:i:"))) {
 	while(-1 != (opt = getopt(argc, argv, "hp:d:b:"))) {
 		switch(opt) {
 		case 'p':	strcpy(ttypath, optarg);			break;
 		case 'd':	strcpy(ttypath1, optarg);			break;
 		case 'b':	op_baudrate = atoi(optarg);			break;
+		case 'h':	print_usage() ; exit(0);			break;
         default:
              break;
         }
@@ -390,35 +389,35 @@ int main (int argc, char **argv)
 	fd1 = open(ttypath1, O_RDWR| O_NOCTTY);	// open TTY Port
 
 	if (0 > fd) {
-  		printf("Fail, open '%s', %s\n", ttypath, strerror(errno));
-  		free(pbuf);
-  		return -1;
-  		//exit(1);
- 	}
+		printf("Fail, open '%s', %s\n", ttypath, strerror(errno));
+		free(pbuf);
+		return -1;
+		//exit(1);
+	}
 
- 	/*
+	/*
 	 * save current termios
 	 */
 	tcgetattr(fd, &oldtio);
 
 	par->fd = fd;
- 	memcpy(&par->tio, &oldtio, sizeof(struct termios));
+	memcpy(&par->tio, &oldtio, sizeof(struct termios));
 
- 	speed = get_baudrate( op_baudrate);
+	speed = get_baudrate( op_baudrate);
 
- 	if(!speed)
- 		goto _exit;
+	if(!speed)
+		goto _exit;
 	memcpy(&newtio, &oldtio, sizeof(struct termios));
 
- 	newtio.c_cflag &= ~CBAUD;	// baudrate mask
- 	newtio.c_iflag 	&= ~ICRNL;
-	newtio.c_cflag |=  speed; 	
-	
+	newtio.c_cflag &= ~CBAUD;	// baudrate mask
+	newtio.c_iflag &= ~ICRNL;
+	newtio.c_cflag |=  speed;
+
 	newtio.c_cflag &=   ~CS8;
 	switch (op_data)
-	{	
+	{
 		case 5:
-			newtio.c_cflag |= CS5; 
+			newtio.c_cflag |= CS5;
 			break;
 		case 6:
 			newtio.c_cflag |= CS6;
@@ -435,32 +434,32 @@ int main (int argc, char **argv)
 			break;
 	}
 
-	newtio.c_iflag 	|= IGNBRK|IXOFF;
+	newtio.c_iflag |= IGNBRK|IXOFF;
 
-	newtio.c_cflag 	&= ~HUPCL;
+	newtio.c_cflag &= ~HUPCL;
 	if(op_flow)
 		newtio.c_cflag |= CRTSCTS;   /* h/w flow control */
     else
-    	newtio.c_cflag &= ~CRTSCTS;  /* no flow control */
-  
-	newtio.c_iflag 	|= 0;	// IGNPAR;
-	newtio.c_oflag 	|= 0;	// ONLCR = LF -> CR+LF
-	newtio.c_oflag 	&= ~OPOST;
+	newtio.c_cflag &= ~CRTSCTS;  /* no flow control */
 
-	newtio.c_lflag 	= 0;
-	newtio.c_lflag 	= op_canoical ? newtio.c_lflag | ICANON : newtio.c_lflag & ~ICANON;	// ICANON (0x02)
-	
-	newtio.c_cflag	= op_stop ? newtio.c_cflag | CSTOPB : newtio.c_cflag & ~CSTOPB;	//Stop bit 0 : 1 stop bit 1 : 2 stop bit
-	
-	newtio.c_cflag	= op_parity ? newtio.c_cflag | PARENB : newtio.c_cflag & ~PARENB; //0: No parity bit, 1: Parity bit Enb , 
-	newtio.c_cflag	= op_odd ? newtio.c_cflag | PARODD : newtio.c_cflag & ~PARODD; //0: No parity bit, 1: Parity bit Enb , 
+	newtio.c_iflag |= 0;	// IGNPAR;
+	newtio.c_oflag |= 0;	// ONLCR = LF -> CR+LF
+	newtio.c_oflag &= ~OPOST;
+
+	newtio.c_lflag = 0;
+	newtio.c_lflag = op_canoical ? newtio.c_lflag | ICANON : newtio.c_lflag & ~ICANON;	// ICANON (0x02)
+
+	newtio.c_cflag = op_stop ? newtio.c_cflag | CSTOPB : newtio.c_cflag & ~CSTOPB;	//Stop bit 0 : 1 stop bit 1 : 2 stop bit
+
+	newtio.c_cflag = op_parity ? newtio.c_cflag | PARENB : newtio.c_cflag & ~PARENB; //0: No parity bit, 1: Parity bit Enb ,
+	newtio.c_cflag = op_odd ? newtio.c_cflag | PARODD : newtio.c_cflag & ~PARODD; //0: No parity bit, 1: Parity bit Enb ,
 
 
 	if (!(ICANON & newtio.c_lflag)) {
-	 		newtio.c_cc[VTIME] 	= op_timeout; 	// time-out °ªÀ¸·Î »ç¿ëµÈ´Ù. time-out = TIME* 100 ms,  0 = ¹«ÇÑ
-	 		newtio.c_cc[VMIN] 	= op_charnum; 	// MINÀº read°¡ ¸®ÅÏµÇ±â À§ÇÑ ÃÖ¼ÒÇÑÀÇ ¹®ÀÚ °³¼ö.
+			newtio.c_cc[VTIME]	= op_timeout;	// time-out °ªÀ¸·Î »ç¿ëµÈ´Ù. time-out = TIME* 100 ms,  0 = ¹«ÇÑ
+			newtio.c_cc[VMIN]	= op_charnum;	// MINÀº read°¡ ¸®ÅÏµÇ±â À§ÇÑ ÃÖ¼ÒÇÑÀÇ ¹®ÀÚ °³¼ö.
 	}
-	
+
 	//dump_termios(1, &oldtio, "OLD TERMIOS");
 	//dump_termios(1, &newtio, "NEW TERMIOS");
 
@@ -475,15 +474,15 @@ int main (int argc, char **argv)
 	UART_DBG("ret = %d \n",ret);
 	if(ret < 0)
 		goto _exit;
-	
+
 	tcflush  (fd, TCIFLUSH);
- 	tcsetattr(fd, TCSANOW, &oldtio);
+	tcsetattr(fd, TCSANOW, &oldtio);
 	tcflush  (fd1, TCIFLUSH);
- 	tcsetattr(fd1, TCSANOW, &oldtio);
+	tcsetattr(fd1, TCSANOW, &oldtio);
 
 	close(fd);
 	close(fd1);
-	
+
 	fsync(fd);
 	fsync(fd1);
 	fd = open(ttypath, O_RDWR| O_NOCTTY);	// open TTY Port
@@ -494,7 +493,6 @@ int main (int argc, char **argv)
 
 	tcflush  (fd1, TCIFLUSH);
 	tcsetattr(fd1, TCSANOW, &newtio);
-	
 
 	ret = test_transfer(fd1,op_mode,op_sbaud,op_ebaud ,fd);
 	UART_DBG("ret = %d \n",ret);
@@ -506,9 +504,9 @@ _exit:
 	 * restore old termios
 	 */
 	tcflush  (fd, TCIFLUSH);
- 	tcsetattr(fd, TCSANOW, &oldtio);
+	tcsetattr(fd, TCSANOW, &oldtio);
 	tcflush  (fd1, TCIFLUSH);
- 	tcsetattr(fd1, TCSANOW, &oldtio);
+	tcsetattr(fd1, TCSANOW, &oldtio);
 	if(fd)
 		close(fd);
 
@@ -516,7 +514,7 @@ _exit:
 		close(fd1);
 
 	if (pbuf)
-		free(pbuf);	
+		free(pbuf);
 
 	printf("===> ret = %d\n", ret );
 	if( ret < 0 )
